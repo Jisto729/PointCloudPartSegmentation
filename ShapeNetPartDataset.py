a202afc05464104562ebd52fc08769ed49968f7c
Jisto729 <fdvorak2002@gmail.com>
2025-02-27 19:19:01 +0100

    fixed __getitem__ method so it returns tensors

--- ShapeNetPartDataset.py
+++ ShapeNetPartDataset.py
@@ -1,3 +1,4 @@
+import torch
 from torch.utils.data import Dataset
 import h5py
 import numpy as np
@@ -9,17 +10,21 @@ class ShapeNetPartDataset(Dataset):
         self.dir = dir
         self.files = []
         self.size = 0
+        debug = False
 
         #debug sizes: train: 800, val: 100, test: 100
         if split == 'train_debug':
             self.files = ['train0.h5']
             self.size = 800
+            debug = True
         elif split == 'val_debug':
             self.files = ['val0.h5']
             self.size = 100
+            debug = True
         elif split == 'test_debug':
             self.files = ['test0.h5']
             self.size = 100
+            debug = True
         elif split == 'train':
             self.files = ['train0.h5','train1.h5','train2.h5','train3.h5','train4.h5', 'train5.h5']
         elif split == 'val':
@@ -27,29 +32,24 @@ class ShapeNetPartDataset(Dataset):
         elif split == 'test':
             self.files = ['test0.h5','test1.h5']
 
-        if self.size == 0:
-            for file in self.files:
-                with h5py.File(os.path.join(self.dir, file), 'r') as f:
+        point_clouds = np.empty((0, 2048, 3))
+        point_labels = np.empty((0, 2048))
+
+        for file in self.files:
+            with h5py.File(os.path.join(self.dir, file), 'r') as f:
+                if not debug:
                     self.size += f["data"].shape[0]
+                point_clouds = np.concatenate((point_clouds, np.array(f["data"])))
+                point_labels = np.concatenate((point_labels, np.array(f["seg"])))
+
+        self.point_clouds = torch.tensor(point_clouds[:self.size], dtype=torch.float32)
+        self.point_labels = torch.tensor(point_labels[:self.size], dtype=torch.long)
 
     def __len__(self):
         return self.size
 
     def __getitem__(self, index):
-        if index >= self.size:
-            raise IndexError("Index out of range")
-        #2048 = num of point clouds in each full h5 file
-        file_index = index // 2048
-        print(file_index)
-        print(index)
-        index = index % 2048
-        print(self.files)
-        curr_file = self.files[file_index]
-        with h5py.File(os.path.join(self.dir, curr_file), 'r') as f:
-            points = np.array(f["data"])
-            parts = np.array(f["seg"])
-
-        point_cloud = points[index]
-        point_labels = parts[index]
+        point_cloud = self.point_clouds[index]
+        point_labels = self.point_labels[index]
 
         return point_cloud, point_labels
